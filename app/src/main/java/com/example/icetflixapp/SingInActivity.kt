f7893243a1c0b60b8d5af26c733f7283da89f7d7
CarlosBitizin <carlospintorossy07@gmail.com>
2022-06-30 18:53:39 -0400

    creation of functions to register user and navigate between screens

--- app/src/main/java/com/example/icetflixapp/SingInActivity.kt
+++ app/src/main/java/com/example/icetflixapp/SingInActivity.kt
@@ -1,11 +1,30 @@
 package com.example.icetflixapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.icetflixapp.databinding.ActivitySingInBinding
 
 class SingInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySingInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sing_in)
+        binding = ActivitySingInBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.RegistrarConta.setOnClickListener({
+            IrParaTelaCadastro()
+        })
+    }
+
+    private fun IrParaTelaCadastro(){
+
+        val navTela = Intent(this,SingUpActivity::class.java)
+        startActivity(navTela)
     }
 }
+
--- app/src/main/java/com/example/icetflixapp/SingUpActivity.kt
+++ app/src/main/java/com/example/icetflixapp/SingUpActivity.kt
@@ -1,11 +1,52 @@
 package com.example.icetflixapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.icetflixapp.databinding.ActivitySingUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SingUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySingUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sing_up)
+        binding = ActivitySingUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.txtConectarConta.setOnClickListener {
+            val navTela = Intent(this,SingInActivity::class.java)
+            startActivity(navTela)
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnCadastrar.setOnClickListener{
+            val email = binding.inputEmail.text.toString()
+            val pass = binding.inputSenha.text.toString()
+            val confirmPass = binding.inputConfirmarSenha.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (pass == confirmPass){
+
+                    firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val intent = Intent(this,SingInActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "A senha não corresponde", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Campos vazios não são permitidos !!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
