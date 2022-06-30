9ef1e38dedc9b7df26d6eb932b2c3ef449d8476a
CarlosBitizin <carlospintorossy07@gmail.com>
2022-06-30 19:06:43 -0400

    creation of the function to access the application (login)

--- app/src/main/java/com/example/icetflixapp/SingInActivity.kt
+++ app/src/main/java/com/example/icetflixapp/SingInActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.example.icetflixapp.databinding.ActivitySingInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SingInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySingInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +19,30 @@ class SingInActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.RegistrarConta.setOnClickListener({
-            IrParaTelaCadastro()
-        })
-    }
+        binding.RegistrarConta.setOnClickListener {
+            val navTela = Intent(this, SingUpActivity::class.java)
+            startActivity(navTela)
+        }
+        firebaseAuth = FirebaseAuth.getInstance()
 
-    private fun IrParaTelaCadastro(){
+        binding.btnEntrar.setOnClickListener{
+            val email = binding.inputEmail.text.toString()
+            val pass = binding.inputSenha.text.toString()
 
-        val navTela = Intent(this,SingUpActivity::class.java)
-        startActivity(navTela)
+            if (email.isNotEmpty() && pass.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this,"Usuario ou senha inválido", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Campos vazios não são permitidos !!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
 
