15d91c25f311160cf33db8b2d9616359b350e159
Carlos Eduardo <carlospintorossy07@gmail.com>
2022-09-13 21:47:42 -0400

    feat: movie creation activity and toolbar

--- app/src/main/java/com/example/icetflixapp/MainActivity.kt
+++ app/src/main/java/com/example/icetflixapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.icetflixapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.icetflixapp.model.Category
@@ -12,8 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
         val categories = mutableListOf<Category>()
         for(j in 0 until 5){
